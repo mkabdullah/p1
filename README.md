4be122c68c3698afe875d1d0f28828923cdf9812
Muhammad Abdullah <kaleem.abdullah@gmail.com>
2016-09-14 21:24:51 -0400

    Added stylesheet and pic

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-
 # P1 -- System Setup / Portfolio
+
+## Where is it?
+
+[Project Link](http://p1.kaleemabdullah.com).
 
 ## Description
 
-Description goes here...
+This is my first project for the course CSCI E-15 Dymanic Web Applications. The goals of this project include setting up a portfolio to present all of my projects that I will do for this course, and showing that my Git and server workflow is working properly. The project is hosted on a Digital Ocean LAMP machine.
+
+## Project Demo
+
+The project demo video is hosted [here](http://video-url-placeholder)
--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<h1>Project 1!</h1>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Muhammad Abdullah - P1 - DWA15 Portfolio</title>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel='stylesheet' href='http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+    <link href="http://netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+
+    <link href='css/main.css' rel='stylesheet'>
+
+</head>
+<body>
+
+    <div class="container">
+
+        <h1>Muhammad Kaleem Abdullah</h1>
+
+        <img src='images/kaleem.jpg' width=300 height=500 alt='Muhammad Abdullah'>
+
+        <h3>Project 2 Title</h3>
+        <p>View: [link]</p>
+        <p>Github: [link]</p>
+
+        <h3>Project 3 Title</h3>
+        <p>View: [link]</p>
+        <p>Github: [link]</p>
+
+        <h3>Project 4 Title</h3>
+        <p>View: [link]</p>
+        <p>Github: [link]</p>
+
+    </div>
+
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
